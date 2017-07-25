4e06c301b2688de0613adcdd6354cccb87f8d75a
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-25 15:15:05 -0400

    Added files and connected them

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
+const data = require('data.js');
 
 const app = express();
 
+app.use(express.static('public'))
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
 //Listening on root
 app.get('/', function (req, res) {
-  res.send('test')
+  res.render('directory-list')
 })
 
 app.listen(3000, function () {
